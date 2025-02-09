4e214b13c28b75c587a58ca7693445696b65993a
agnivad <agnivad@gmail.com>
2025-02-09 23:51:53 +0800

    update

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from src.config.settings import settings
-from src.data_sources.alpha_vantage import AlphaVantageFetcher
-from src.data_sources.cryptoquant import CryptoQuantFetcher
-from src.data_sources.birdeye import BirdeyeFetcher
+from src.fetchers.alpha_vantage import AlphaVantageFetcher
+from src.fetchers.cryptoquant import CryptoQuantFetcher
+from src.fetchers.birdeye import BirdeyeFetcher
 import json
 import logging
 
@@ -11,15 +11,25 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     # Initialize fetchers
     alpha_vantage_fetcher = AlphaVantageFetcher(settings.ALPHA_VANTAGE_API_KEY)
-    crypto_quant_fetcher = CryptoQuantFetcher(settings.CRYPTOQUANT_API_KEY)
+    #crypto_quant_fetcher = CryptoQuantFetcher(settings.CRYPTOQUANT_API_KEY)
     birdeye_fetcher = BirdeyeFetcher(settings.BIRDEYE_API_KEY)
 
     # Fetch data
-    alpha_vantage_data = alpha_vantage_fetcher.fetch_data("TIME_SERIES_INTRADAY", {"symbol": "IBM", "interval": "5min"})
+    alpha_vantage_params = {"symbol": "BTC", "market": "USD"}
+    alpha_vantage_data = alpha_vantage_fetcher.fetch_data(
+        endpoint="DIGITAL_CURRENCY_DAILY", 
+        params=alpha_vantage_params
+    )
     logger.info("Alpha Vantage Data: %s", json.dumps(alpha_vantage_data, indent=2))
 
-    crypto_quant_data = crypto_quant_fetcher.fetch_data("market/exchange-flows", {"market": "binance"})
-    logger.info("CryptoQuant Data: %s", json.dumps(crypto_quant_data, indent=2))
+    #crypto_quant_data = crypto_quant_fetcher.fetch_data("market/exchange-flows", {"market": "binance"})
+    #logger.info("CryptoQuant Data: %s", json.dumps(crypto_quant_data, indent=2))
 
-    birdeye_data = birdeye_fetcher.fetch_data("defi/overview")
+    birdeye_data_params = {"address": "So11111111111111111111111111111111111111112", "check_liquidity":100}
+    birdeye_headers = {"x-chain": "solana"}
+    birdeye_data = birdeye_fetcher.fetch_data(
+        endpoint="defi/price",
+        params=birdeye_data_params,
+        headers=birdeye_headers
+    )
     logger.info("Birdeye Data: %s", json.dumps(birdeye_data, indent=2))
